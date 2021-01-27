113b8caede0cf3897467599f707faaa8280731d6
Miguel Simon Martinez <mscm21@yahoo.com>
2021-01-26 21:57:08 -0500

    Add project files.

---
+++ SecurityLab1_Starter/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityLab1_Starter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var appSettings = ConfigurationManager.AppSettings;
+            var colour = appSettings["colour"];
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DefaultAction() {
+            return View();
+        }
+
+
+        public ActionResult NotFound() {
+
+            return View();
+        }
+
+        public ActionResult GenError() {
+            return new HttpStatusCodeResult(500);
+            throw new Exception();
+            throw new DivisionByZeroException();
+
+        }
+    }
+
+	[Serializable]
+	internal class DivisionByZeroException : Exception
+	{
+		public DivisionByZeroException()
+		{
+		}
+
+		public DivisionByZeroException(string message) : base(message)
+		{
+		}
+
+		public DivisionByZeroException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected DivisionByZeroException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
